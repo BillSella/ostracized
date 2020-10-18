65d8f346b056fb07aa02e448b25cceae8b29237c
Bill Sella <bill.sella@gmail.com>
2020-10-18 08:44:27 -0700

    Added integer decoder callbacks.

--- src/decode_tlv.h
+++ src/decode_tlv.h
@@ -16,33 +16,163 @@
 	  buffer   - The supplied buffer holding the TLV to decode.
 	  length   - The supplied buffer length.
 
-	  callback - The callback function to execute after decoding.
-	    value  - The value buffer.
-	    length - The decoded value length.
-	    type   - The data type.
-	    tag    - The data type tag.
+	  callback  - The callback function to execute after decoding.
+	    value   - The value buffer.
+	    length  - The decoded value length.
+	    type    - The data type.
+	    tag     - The data type tag.
+	    context - A generic context pointer.
+
+	  context   - A generic context pointer to pass to the callback.
 
 	Returns the result of callback on success or a negative value on error.
 	  -1 - Buffer contained insufficient bytes to decode.
 */
-int decode_tlv(const char * buffer, ssize_t length, int callback(const char * value, ssize_t length, uint8_t type, uint64_t tag));
-
-int decode_str(const char * buffer, ssize_t length, int callback(const char * value));
-
-int decode_oid(const char * buffer, ssize_t length, int callback(const char * value));
-
-int decode_ip4(const char * buffer, ssize_t length, int callback(const char * value));
-
-int decode_ip6(const char * buffer, ssize_t length, int callback(const char * value));
+int decode_tlv(const char * buffer, ssize_t length, int callback(const char * value, ssize_t length, uint8_t type, uint64_t tag, void * context), void * context);
 
 /*
-	Decode a BER encoded integer (INTEGER, Integer32, Unsigned32, Counter32, Counter64, Gague32, TimeTicks).
+	Unpacks a sequence of bytes into a signed 8-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
 */
-int decode_s64(const char * buffer, ssize_t length, int callback(uint64_t value));
+int unpack_s08(const char * value, ssize_t length, uint8_t type, uint64_t tag, int8_t * result);
 
 /*
-	Decode a BER encoded integer (INTEGER, Integer32, Unsigned32, Counter32, Counter64, Gague32, TimeTicks).
+	Unpacks a sequence of bytes into a signed 16-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
 */
-int decode_u64(const char * buffer, ssize_t length, int callback(uint64_t value));
+int unpack_s16(const char * value, ssize_t length, uint8_t type, uint64_t tag, int16_t * result);
+
+/*
+	Unpacks a sequence of bytes into a signed 32-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
+*/
+int unpack_s32(const char * value, ssize_t length, uint8_t type, uint64_t tag, int32_t * result);
+
+/*
+	Unpacks a sequence of bytes into a signed 64-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
+*/
+int unpack_s64(const char * value, ssize_t length, uint8_t type, uint64_t tag, int64_t * result);
+
+/*
+	Unpacks a sequence of bytes into a unsigned 8-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
+*/
+int unpack_u08(const char * value, ssize_t length, uint8_t type, uint64_t tag, uint8_t * result);
+
+/*
+	Unpacks a sequence of bytes into a unsigned 16-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
+*/
+int unpack_u16(const char * value, ssize_t length, uint8_t type, uint64_t tag, uint16_t * result);
+
+/*
+	Unpacks a sequence of bytes into a unsigned 32-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
+*/
+int unpack_u32(const char * value, ssize_t length, uint8_t type, uint64_t tag, uint32_t * result);
+
+/*
+	Unpacks a sequence of bytes into a unsigned 64-bit value.
+
+	Parameters
+	  value    - The supplied buffer holding the sequence of bytes to unpack.
+	  length   - The supplied buffer length.
+
+	  type     - The TLV type.
+	  tag      - The TLV tag.
+
+	  result   - A pointer to the variable into which to write the unpacked value.
+
+	Returns zero on success or a negative error code on failure.
+	  -1 - Buffer contains too many bytes to upack into the supplied result buffer.
+	  -2 - The supplied result buffer is NULL.
+*/
+int unpack_u64(const char * value, ssize_t length, uint8_t type, uint64_t tag, uint64_t * result);
 
 #endif
+
---
+++ src/unpack_tlv.s
@@ -0,0 +1,139 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Author:  Bill Sella <bill.sella@gmail.com>
+; Updated: 2020-08-30
+; License: GPL 2.0
+;
+; Define global exports for public function calls in this module.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+global  unpack_s08
+global  unpack_s16
+global  unpack_s32
+global  unpack_s64
+global  unpack_u08
+global  unpack_u16
+global  unpack_u32
+global  unpack_u64
+section .text
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Functions to unpack a sequence of bytes into integers of various lengths.
+;
+; Prototypes
+;    int unpack_s08(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, int8_t * result
+;    );
+;    int unpack_s16(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, int16_t * result
+;    );
+;    int unpack_s32(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, int32_t * result
+;    );
+;    int unpack_s64(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, int64_t * result
+;    );
+;    int unpack_u08(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, uint8_t * result
+;    );
+;    int unpack_u16(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, uint16_t * result
+;    );
+;    int unpack_u32(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, uint32_t * result
+;    );
+;    int unpack_u64(const char * value, ssize_t length, uint8_t type,
+;       uint64_t tag, uint64_t * result
+;    );
+;
+; Parameters
+;    (rdi) value    - The sequence of bytes to upack.
+;    (rsi) length   - The length of the value buffer.
+;
+;    (rdx) type     - The first byte of the BER encoded TLV type field.
+;    (rcx) tag      - The tag field of the TLV type.
+;     (r9) result   - A pointer to the integer to unpack the value into.
+;
+; Returns zero on success or a negative error code on failure.
+;    -1 - Insufficient data in buffer to decode.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+unpack_s08:
+unpack_u08:
+   cmp    rsi,   1        ; Ensure the length is not more than 8 bits.
+   js     error.e_length  ;
+
+   mov    rdx,  exit.i08  ; Set the function exit point and jump to the main
+   jmp    unpack_int      ; code block.
+
+unpack_s16:
+unpack_u16:
+   cmp    rsi,   2        ; Ensure the length is not more than 16 bits.
+   js     error.e_length  ;
+
+   mov    rdx,  exit.i16  ; Set the function exit point and jump to the main
+   jmp    unpack_int      ; code block.
+
+unpack_s32:
+unpack_u32:
+   cmp    rsi,   4        ; Ensure the length is not more than 32 bits.
+   js     error.e_length  ;
+
+   mov    rdx,  exit.i32  ; Set the function exit point and jump to the main
+   jmp    unpack_int      ; code block.
+
+unpack_s64:
+unpack_u64:
+   cmp    rsi,   8        ; Ensure the length is not more than 64 bits.
+   js     error.e_length  ;
+
+   mov    rdx,  exit.i64  ; Set the function exit point and jump to the main
+
+unpack_int:
+   or      r9,  r9        ; Ensure the supplied value is not NULL.
+   jz     error.e_result  ;
+
+   add    rdi, rsi        ; Advance to the last byte.
+   xor    rax, rax        ; Zero the result value.
+
+   .loop:
+   cmp    rdi, rsi        ;
+   jz     exit            ; Determine if all bytes are copied.
+
+   shl    rax, 8          ; Shift the next byte into the result.
+
+   mov     al, [rdi]      ; Copy the byte and loop.
+   dec    rdi             ; Advanced to next byte.
+
+   jmp    .loop           ;
+
+exit:
+   jmp    rdx             ; Jump to the appropriate exit block. TODO: adjust to
+                          ; self modifying code to execute the appropriate
+                          ; machine language section within a db and remove the
+                          ; jump from the execution path.
+
+   .i08:
+   mov   [r9], al         ;
+   retn                   ;
+
+   .i16:
+   mov   [r9], ax         ;
+   retn                   ;
+
+   .i32:
+   mov   [r9], eax        ;
+   retn                   ;
+
+   .i64:
+   mov   [r9], rax        ;
+   retn                   ;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Error condition table.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+error:
+   .e_length:
+   mov    rax, -1         ; Set the error code for insufficient output length.
+   retn                   ; Return to caller.
+
+   .e_result:
+   mov    rax, -2         ; Set the error code for NULL result pointer.
+   retn                   ; Return to caller.
